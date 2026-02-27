0dce2cf123969765954807739cc0630d02759b9e
Lcbx <luc.courbariaux@gmail.com>
2026-02-28 00:53:01 +0100

    tweak physics

--- character.gd
+++ character.gd
@@ -14,12 +14,13 @@ static func const_lerp(a, b, amount):
 func _ready() -> void:
 	animation_player.play("idle")
 
-@export var SPEED = 250.0
-@export var CONTROL = 20.0
-@export var AIR_CONTROL = 9.0
-@export var JUMP_VELOCITY = 330.0
-@export var GRAVITY = 80.0
-const ON_GROUND_MARGIN = 10.0
+@export var SPEED := 350.0
+@export var CONTROL := 20.0
+@export var AIR_CONTROL := 10.0
+@export var GRAVITY := 500.0
+@export var MAX_FALL_SPEED := 250.0
+@export var JUMP_VELOCITY = 400.0
+const ON_GROUND_MARGIN := 10.0
 
 var grounded : bool = false
 var direction := Vector2.ZERO
@@ -28,8 +29,7 @@ var direction := Vector2.ZERO
 # TODO: move input out of charcter
 func _physics_process(delta: float) -> void:
 	grounded = ground_check.is_colliding()
-	var accel = (CONTROL if grounded else AIR_CONTROL) * delta
-	var gravity_accel = GRAVITY * 0.5 * delta
+	var accel := (CONTROL if grounded else AIR_CONTROL) * delta
 
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	sprite.scale.x = sign(velocity.x) if velocity.x != 0.0 else sprite.scale.x
@@ -44,12 +44,16 @@ func _physics_process(delta: float) -> void:
 		elif dist.length_squared() < ON_GROUND_MARGIN:
 			velocity.y = 0.0
 		else:
-			velocity.y = -GRAVITY*0.5
+			velocity.y = -MAX_FALL_SPEED
 		#print("dist ", dist)
+		move_and_slide()
 	else:
+		var y_dir := -1.0 if Input.is_action_pressed("ui_accept") else direction.y
+		var gravity_accel := GRAVITY * (1.0 + 0.5 * y_dir) * delta
+		# applying half acceleration before + after movement makes for a better integration of the force
 		velocity.y += gravity_accel
-		var y_dir = -1.0 if Input.is_action_pressed("ui_accept") else direction.y
-		velocity.y = const_lerp(velocity.y, velocity.y + GRAVITY * (1 + 0.5 * y_dir), accel)
+		velocity.y = min(MAX_FALL_SPEED * (1.0 + 0.34 * y_dir), velocity.y)
+		move_and_slide()
+		velocity.y += gravity_accel
 
-	move_and_slide()
 	
