6edaca14abbff2f60151526b2f1f40f00812dfa3
Lcbx <luc.courbariaux@gmail.com>
2026-04-09 23:42:42 +0200

    split power into damage, impulse and hitstun

--- scripts/State.gd
+++ scripts/State.gd
@@ -7,29 +7,24 @@ class_name State
 @abstract func enter(...args)->void
 @abstract func apply(delta: float)->void
 
-# applioed when receiving damage
-@export var damage_impulse_mult :float= 0.07
-@export var damage_hitstun_mult :float= 0.02
-@export var longstun_duration :float= 1.0
-
 @onready var HITSTUN_STATE : State = %HitstunState
 
 # TODO :
 # * add hitstun state / calculate hitstun duration
 # * apply hyperarmor
 # * add hard stun when health dips under 0
-func receive_damage(power:float, dir:Vector2)->void:
-	chtr.health -= power
-	# assuming max health is 100, impulse is doubled at 0 health
-	var impulse_value = (200.0 - chtr.health) * damage_impulse_mult * power * power
-	chtr.impulse += dir * impulse_value
+func receive_damage(damage:float, impulse:Vector2, hitstun)->void:
+	if damage>0.0:
+		chtr.health -= damage
 	
-	var hitstun_duration:float = clampf(damage_hitstun_mult * power, 0.0, 0.4)
+	# assuming max health is 100, impulse is doubled at 0 health
+	chtr.impulse += (200.0 - chtr.health) * impulse
 	
 	# TODO : add visual indicator of long stun
 	# maybe make that be handled by hitstun state, with stronger visual with longer stuns
-	if chtr.health < 0.0: hitstun_duration = longstun_duration
+	var hitstun_duration:float = hitstun if chtr.health > 0.0 else 1.0
 	
 	print(chtr.name, ' health ', chtr.health, ' impulse ', chtr.impulse, ' hitstun ', hitstun_duration)
-	chtr.enter(HITSTUN_STATE, hitstun_duration)
+	if hitstun_duration > 0.0:
+		chtr.enter(HITSTUN_STATE, hitstun_duration)
 	
--- scripts/dmg_area.gd
+++ scripts/dmg_area.gd
@@ -2,14 +2,19 @@
 extends Area2D
 class_name DmgArea
 
-@export var power : float = 5.0 :
+@export var damage : float = 5.0 :
 	set(value):
-		power = value
+		damage = value
 		update()
 
-@export var impulse : float = 0.0 :
+@export var impulse : float = 2.0 :
 	set(value):
 		impulse = value
+		update()
+
+@export var hitstun : float = 0.1 :
+	set(value):
+		hitstun = value
 		update()
 
 @export var angle : float = -10.0 :
@@ -18,16 +23,21 @@ class_name DmgArea
 		update()
 
 func update()->void:
-	_dir = Vector2.RIGHT.rotated(deg_to_rad(angle))
-	update_line(power, Color.BLUE_VIOLET)
-	update_line(impulse, Color.BISQUE)
+	update_line(damage, Color.RED)
+	update_line(impulse, Color.BLUE)
+	update_line(hitstun * 20.0, Color.GREEN)
 
 @export var disable_time : float = 0.17
 
-var _dir : Vector2
+var _impulse : Vector2
 
 func _ready() -> void:
-	update()
+	_impulse = Vector2.RIGHT.rotated(deg_to_rad(angle)) * impulse
+	if Engine.is_editor_hint():
+		for c in self.get_children():
+			if c.name.begins_with(debugLineName):
+				c.queue_free()
+		update()
 
 var _chtr : Character
 func register(chtr:Character, ...args)->void:
@@ -36,17 +46,16 @@ func register(chtr:Character, ...args)->void:
 
 func handle(body:CollisionObject2D)->void:
 	var victim := body as Character
-	var impulse_dir = _dir * _chtr.collision.scale
+	var final_impulse = _impulse * _chtr.collision.scale 
 	
 	if victim and victim != _chtr:
 		#print('hi', body.name, " from ", _chtr.name)
 		
-		if power > 0.0:
-			victim._state.receive_damage(power, impulse_dir)
-			temp_disable.call_deferred()
-		
-		if impulse > 0.0:
-			victim.impulse += impulse_dir * impulse * impulse
+		if damage > 0.0:
+			victim._state.receive_damage(damage, final_impulse, hitstun)
+			#temp_disable.call_deferred()
+		else:
+			victim.impulse += final_impulse
 
 func temp_disable()->void:
 		self.monitoring = false 
@@ -54,18 +63,23 @@ func temp_disable()->void:
 		func():self.monitoring = true
 		))
 
+const debugLineName := "debug_line_"
+const debugLineSize := 1.5
+const debugLineLength := 5.0
+
 func update_line(value:float, color:Color)->void:
 	if Engine.is_editor_hint() and value > 0.0:
-		var name := "debug_" + str(color)
+		var name := debugLineName + color.to_html()
 		var line := self.get_node_or_null(name) as Line2D
 		if line == null:
 			line = Line2D.new()
 			line.name = name
 			self.add_child(line)
 		line.clear_points()
-		var rads = deg_to_rad(angle)
 		line.default_color = color
-		line.add_point(Vector2.ZERO)
-		line.add_point(Vector2(cos(rads), sin(rads)) * value * 5.0)
-		line.width = 3.0
+		var index:int = get_parent().get_child_count() - line.get_index()
+		var start = Vector2.RIGHT.rotated(deg_to_rad(angle + 90)) * debugLineSize * index
+		line.add_point(start)
+		line.add_point(start + _impulse * value * debugLineLength)
+		line.width = debugLineSize
 		line.z_index = 1
